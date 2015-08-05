b9d03a2c18ea54f4d06109759f728adaf185c17c
Alessando Cucci <alessandro.cucci@energee3.com>
2015-08-05 11:30:12 +0200

    qui inizia Energee3PythonTools

---
+++ README.md
@@ -0,0 +1,35 @@
+# ENERGEE3 PYTHON TOOLS
+  Collezione di script e documentazione per sviluppatori Python
+
+
+> In the face of ambiguity, refuse the temptation to guess.
+
+
+
+### Version
+0.0.1
+
+### Tech
+
+Principalmente questo repository si compone di due parti:
+
+* Documentazione
+  * Python code style
+  * Tutorials
+  * Ottimizzazione del codice
+* Collezione di Script
+  * Esempi di codice riutilizzabile (algoritmi, strutture dati, design patterns)
+  * Tools vari (performance, esempi)
+
+### What's New
+- Speed Test
+
+### Todo's
+ - Iniziare a scrivere la documentazione
+ - Creare un indice dettagliato
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+'''
+Energee3 Python Tools
+
+Ai colleghi di Energee3:
+come suggerisce il nome, si tratta di un insieme di script e tool vari che potrebbero tornare utili nello
+sviluppo di codice Python.
+
+PS Pull request ben accette!
+'''
+
+__author__ = "Alessandro Cucci"
+__package_name__ = "Energee3 Python Tools"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__email__ = "alessandro.cucci@energee.com"
+__status__ = "Development"
+
+import argparse
+
+
+def performance():
+    import timeit
+    timeit.main()
+
+
+if __name__ == '__main__':
+    logo = """
+
+                             .;;;,
+                             ;;;;;                          .::
+                            `;;;;;                         ;;;;;;
+                             ;;;;;                        ;;;;;;;:
+                             `;;;                         ;;;;;;;;
+                               ;;                        .;;;;;;;;
+                               ;;                        ;;;;;;;;;
+                               :;                       `;;;;;;;;
+                               .;                      `;;;;;;;:
+                                ;`                    .;;;;;:`
+                                ;:                   :;;;`
+                                ;;                  ;;;.
+                                ;;                .;;;
+       :;;;                     ,;               ;;;.
+     `;;;;;;,                   `;`            `;;;
+     ;;;;;;;;                    ;:           :;;.
+    `;;;;;;;;;                   ;;          ;;;
+    .;;;;;;;;;;`                 ;;        ,;;,
+     ;;;;;;;;;;;;;.              ;;,      ;;;
+     ;;;;;;;;;;;;;;;;:`          :;;    .;;;
+     ,;;;;;;;`.,;;;;;;;;;,       ,;;.  ;;;,
+      `;;;;,       `:;;;;;;;;`   ,;;;;;;;`
+                        .;;;;;;;:;;;;;;;
+                            ,;;;;;;;;;;;.
+                               ,;;;;;;;;;;;:.       ,.
+                                ;;;;;;;,:;;;;;;;. :;;;;
+                               ;;;;;;;.     `,:;;;;;;;;;
+                             `;;,  :;;:          ;;;;;;;
+                            :;;     ;;;           ;;;;;
+                           ;;`      `;;           `;;;`
+                         .;:         ;;`
+                        ;;`           ;;
+                   .;;:;;             ;;
+                   ;;;;:               ;.
+                   ;;;;`               ;;
+                   ,;;;                 ;`
+                     .                  ;;;
+                                        ;;;
+                                        ;;;
+
+
+                       ENERGEE3 PYTHON TOOLS
+                            Energee3 srl
+
+    """
+    print logo
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', '--performance', action='store_true',
+                            help='Execute timeit.py script')
+
+    parser.add_argument('--version', action='version', version=' '.join([__package_name__, __version__]))
+
+    results = parser.parse_args()
+    if results.performance:
+        performance()
+
+
